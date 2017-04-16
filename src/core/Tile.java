d10c89e57f021ddb9d8352c7e0cdcf387a364e89
VoltaicRainbow <andrewlrabbit@gmail.com>
2017-04-16 20:06:36 +0100

    Remove debug tile image

--- src/core/Tile.java
+++ src/core/Tile.java
@@ -1,11 +1,7 @@
 package core;
 
-import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
@@ -13,14 +9,15 @@ public class Tile extends JLabel {
     private static final long serialVersionUID = 1L;
 
     public Tile() {
-        BufferedImage img = null;
-        try {
-            img = ImageIO.read(new File("notes.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        setBackground(Color.GRAY);
-        setIcon(new ImageIcon(img));
+
+        // BufferedImage img = null;
+        // try {
+        // img = ImageIO.read(new File("notes.png"));
+        // } catch (IOException e) {
+        // e.printStackTrace();
+        // }
+        // setBackground(Color.GRAY);
+        // setIcon(new ImageIcon(img));
         // System.out.println("tile drawn");
         setVisible(true);
     }
