8a084d2f557a043ceee4db5d79e9a8bd39fae940
Andrew Luckett <andyluckett9@gmail.com>
2018-10-19 20:43:24 +0100

    Change logging and loglevel to be clearer Lower level = more important

--- src/main/Debuglog.java
+++ src/main/Debuglog.java
@@ -4,15 +4,14 @@ public class Debuglog {
 
     /**
      * loglevel is the value a log is required to have before it is written
-     * loglevel 5 should equal no logging
+     * The lower the level the more important
+     * negative loglevel should equal no logging
+     * loglevel MAX_INT allows all logs
      */
-    public static int loglevel = 5;
+    public static int loglevel = -1;
 
-    /**
-     * higher level equals higher importance
-     */
     public static void log(String log, int level) {
-        if (level >= loglevel) {
+        if (level <= loglevel && level >= 0) {
             System.out.println(log);
         }
     }
--- src/main/Main.java
+++ src/main/Main.java
@@ -8,7 +8,7 @@ import window.WindowFrame;
 public class Main {
 
     public static void main(String[] args) {
-        Debuglog.loglevel = 5;
+        Debuglog.loglevel = -1;
 
         WindowFrame inst = new WindowFrame(new Home(), new Dimension(1280, 720), "Fractal Viewer - Andrew Luckett 2017");
         inst.setVisible(true);
